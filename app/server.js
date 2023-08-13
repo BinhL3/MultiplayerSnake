247b50190ee4f9ccbdca6bdf58358cfc3e728eb3
bql23 <bql23@drexel.edu>
2023-08-13 02:56:00 -0400

    set up basic socket.io package with clients connecting to server

--- app/server.js
+++ app/server.js
@@ -3,8 +3,17 @@ let express = require("express");
 let app = express();
 let port = 3000;
 let hostname = "localhost";
+
 app.use(express.static("public"));
 
-app.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}`);
-});
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+      console.log('user disconnected');
+    });
+  });
+server.listen(8081, function () {
+    console.log(`Listening on ${server.address().port}`);
+  });
