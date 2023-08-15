f8e29f548f89cea67122959846cac9a50e2d519f
John Ferraioli <ferraiolijohn@gmail.com>
2023-08-15 11:40:54 -0400

    fixed indentation in server.js.

--- app/server.js
+++ app/server.js
@@ -1,33 +1,33 @@
 let express = require("express");
- let http = require('http');
- let socketIO = require('socket.io');
+let http = require('http');
+let socketIO = require('socket.io');
 
- let app = express();
- let port = 3000;
- let hostname = "localhost";
+let app = express();
+let port = 3000;
+let hostname = "localhost";
 
- app.use(express.static("public"));
+app.use(express.static("public"));
 
- var server = http.createServer(app);
- var io = socketIO(server);
+var server = http.createServer(app);
+var io = socketIO(server);
 
- var players = {};
+var players = {};
 
-  io.on('connection', function (socket) {
-      let playerId = socket.id;
-      console.log(`Player ${playerId} connected`);
-      players[playerId] = {
-        x: Math.floor(Math.random() * 800),
-        y: Math.floor(Math.random() * 600),
-        playerId: playerId
-      }
-      socket.emit('currentPlayers', players);
-      socket.on('disconnect', () => {
-        delete players[playerId];
-        console.log(`Player ${playerId} disconnected`);
-      });
- });
+io.on('connection', function (socket) {
+  let playerId = socket.id;
+  console.log(`Player ${playerId} connected`);
+  players[playerId] = {
+    x: Math.floor(Math.random() * 800),
+    y: Math.floor(Math.random() * 600),
+    playerId: playerId
+  }
+  socket.emit('currentPlayers', players);
+  socket.on('disconnect', () => {
+    delete players[playerId];
+    console.log(`Player ${playerId} disconnected`);
+  });
+});
 
- server.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}`);
- });
+server.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}`);
+});
