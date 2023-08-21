ebea1e6d8185e1c1d322374d646f19ce0fd82646
John Ferraioli <ferraiolijohn@gmail.com>
2023-08-21 17:24:11 -0400

    added collision handling and randomizing spawn position while ensuring snakes don't spawn on top of eachother (not finished)

--- app/server.js
+++ app/server.js
@@ -1,33 +1,73 @@
 let express = require("express");
- let http = require('http');
- let socketIO = require('socket.io');
+let http = require('http');
+let socketIO = require('socket.io');
 
- let app = express();
- let port = 3000;
- let hostname = "localhost";
+let app = express();
+let port = 3000;
+let hostname = "localhost";
 
- app.use(express.static("public"));
+app.use(express.static("public"));
 
- var server = http.createServer(app);
- var io = socketIO(server);
+let server = http.createServer(app);
+let io = socketIO(server);
 
- var players = {};
+let players = {};
 
-  io.on('connection', function (socket) {
-      let playerId = socket.id;
-      console.log(`Player ${playerId} connected`);
-      players[playerId] = {
-        x: Math.floor(Math.random() * 800),
-        y: Math.floor(Math.random() * 600),
-        playerId: playerId
-      }
-      socket.emit('currentPlayers', players);
-      socket.on('disconnect', () => {
-        delete players[playerId];
-        console.log(`Player ${playerId} disconnected`);
+function getRandomPosition() {
+  let maxX = 800;
+  let maxY = 600;
+  return {
+    x: Math.floor(Math.random() * maxX),
+    y: Math.floor(Math.random() * maxY)
+  };
+}
+
+function checkCollision(position) {
+  for (let playerId in players) {
+    let player = players[playerId];
+    if (player.x === position.x && player.y === position.y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+io.on('connection', function (socket) {
+    let playerId = socket.id;
+    console.log(`Player ${playerId} connected`);
+
+    let spawnPosition;
+    do {
+      spawnPosition = getRandomPosition();
+    } while (checkCollision(spawnPosition));
+
+    players[playerId] = {
+      x: spawnPosition.x,
+      y: spawnPosition.y,
+      movements: [],
+      playerId: playerId
+    }
+    socket.emit('playerId', playerId);
+
+    socket.on('movementUpdate', (data) => {
+      players[playerId].x = data.x;
+      players[playerId].y = data.y;
+      players[playerId].movements.push({ x: data.x, y: data.y });
+  
+      socket.broadcast.emit('playerPositionUpdate', {
+        playerId,
+        x: data.x,
+        y: data.y,
+        movements: players[playerId].movements
       });
- });
+    });
 
- server.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}`);
- });
+    socket.on('disconnect', () => {
+      delete players[playerId];
+      console.log(`Player ${playerId} disconnected`);
+    });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}`);
+});
